61411bbe95ea1e4bcb8ec7fdb16de781e9cd0608
HubTech <87226674+hubtechsolutions@users.noreply.github.com>
2022-04-23 13:20:39 +0530

    Update constants.js

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to Fredo's NFT drop!!";
 const welcome_h2 = "Connect to MetaMask to Get Started ";
-const welcome_p = 'The Fredo Bang NFT decentralized is a limted NFT drop -- no more than 44 will ever be minted. Follow step by step on the <a href="https://youtube.com/"  target="_blank" rel="noopener noreferrer">FredoBangNFT</a> YouTube channel.';
+const welcome_p = 'The Fredo Bang NFT decentralized is a limted NFT drop -- no more than 44 will ever be minted.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
@@ -13,13 +13,13 @@ const p_presale_coming_soon = "We are working hard to launch the NFT Collection.
 const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
 const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
 const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
+const p_public_mint = "No whitelist needed. Fredo's NFT is now open! 🎉";
 const button_presale_coming_soon = "Get on the Whitelist";
 const button_presale_mint_whitelisted = "Mint Your Special NFT";
 const button_presale_mint_not_whitelisted = "Get on the Whitelist";
 const button_presale_already_minted = "Join The Community";
-const button_public_mint = "Mint Your NFT NOW";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const button_public_mint = "Mint Your NFT";
+const mint_failed = "Minting failed. 😢  Please try again.";
 
 const abi = [
   {
